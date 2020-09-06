2b9a40dbc166b3976683eaae8175810739f9118d
Ricardo Camacho <camacho.rc@gmail.com>
2020-09-06 19:30:19 -0300

    cors origin frontend heroku added

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: 'http://localhost:8080',
+    origin: 'https://grades-frontend-rscamacho.herokuapp.com',
   })
 );
 
