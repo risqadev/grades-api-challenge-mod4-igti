21b966463d69a88797c1cbc01da7e7fdaa90dc68
Ricardo Camacho <camacho.rc@gmail.com>
2020-09-06 22:13:30 -0300

    cors liberado

--- app.js
+++ app.js
@@ -23,11 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: 'http://localhost:8080',
-    origin: 'http://localhost:3000',
-    origin: 'https://grades-frontend-rscamacho.herokuapp.com',
-  })
+  cors()
 );
 
 app.get('/', (_req, res) => {
